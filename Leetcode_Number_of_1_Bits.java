77f570aca23341fb95473ebcae85a1c6153792b0
ananthashankar <anantha.shankar1990@gmail.com>
2015-07-06 20:25:52 -0400

    July 6

---
+++ Leetcode_Number_of_1_Bits.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    
+    
+    public static int hammingWeight(int n) {
+        int cnt = 0;
+        
+        for(int i = 0; i< 32; i++){
+            if((n & 1) == 1){
+                cnt++;
+            }
+            n >>>= 1;
+        }
+        
+        return cnt;
+        // return Integer.bitCount(n);
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int num = Integer.parseUnsignedInt(in.nextLine());
+        System.out.print(num + "\n");
+        int res =  hammingWeight(num);
+        System.out.println(res);
+    }
+    
+}

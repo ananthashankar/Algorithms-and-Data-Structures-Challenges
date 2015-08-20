c5ac46370c43268d0d01ae6c93ecba36dd7ec46a
ananthashankar <anantha.shankar1990@gmail.com>
2015-08-20 17:15:52 -0400

    August 20

---
+++ Reverse_Words.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// not clearing all test cases. needs to be debugged
+
+public class Solution {
+    
+    public static String reverseWords(String s){
+        String res = "";
+        if(s.trim().length() == 0){
+            return res;
+        }
+        String[] tmp = s.split(" ");
+        for(int i = tmp.length - 1; i >= 0; i--){
+            res += tmp[i] + " ";
+        }
+        
+        return res.trim();
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String res =  reverseWords(s);
+        System.out.println(res);
+    }
+}
+

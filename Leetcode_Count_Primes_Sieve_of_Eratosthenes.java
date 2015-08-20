1aa13e2d56389a1d8b479971037c610a1c117438
ananthashankar <anantha.shankar1990@gmail.com>
2015-08-19 21:18:21 -0400

    August 19

--- Leetcode_Count_Primes_Sieve_of_Eratosthenes.java
+++ Leetcode_Count_Primes_Sieve_of_Eratosthenes.java
@@ -7,68 +7,32 @@ import java.util.regex.*;
 
 public class Solution {
     
-    public static List<String> summaryRanges(int[] nums) {
-        List summ = new ArrayList<String>();
-        if(nums.length == 0){
-            return summ;
+    public int countPrimes(int num) {
+        if(num <= 2){
+            return 0;
         }
-        if(nums.length == 1){
-            summ.add(String.valueOf(nums[0]));
-            return summ;
+        int cnt = 0;
+        boolean[] tmp = new boolean[num];
+        for(int i = 0; i<num; i++){
+            tmp[i] = true;
         }
-        int frst = nums[0];
-        int lst = nums[0];
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] == lst + 1){
-                lst = nums[i];
-            } else{
-                if(i + 1 < nums.length){
-                    if(lst > frst){
-                        summ.add(String.valueOf(frst) + "->" + String.valueOf(lst));
-                    } else {
-                        summ.add(String.valueOf(frst));
-                    }
-                    frst = nums[i];
-                    lst = nums[i];
-                    continue;
-                } else if(i+1 == nums.length){
-                    if(lst > frst){
-                        summ.add(String.valueOf(frst) + "->" + String.valueOf(lst));
-                        summ.add(String.valueOf(nums[i]));
-                        lst = frst;
-                        break;
-                    } else {
-                        summ.add(String.valueOf(frst));
-                        summ.add(String.valueOf(nums[i]));
-                        lst = frst;
-                        break;
-                    }
+        
+        for(int i = 2; i < num; i++){
+            if(tmp[i]){
+                cnt++;
+                for(int j = 2*i; j<num; j+=i){
+                    tmp[j]=false;
                 }
             }
-            
         }
         
-        if(summ.size() == 0 ){
-            summ.add(String.valueOf(nums[0]) + "->" + String.valueOf(nums[nums.length - 1])) ;
-            return summ;
-        }
-        if(lst > frst){
-            summ.add(String.valueOf(frst) + "->" + String.valueOf(lst));
-        }
-        
-        return summ;
-        
+        return cnt;
     }
-    
     
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         int num = Integer.parseInt(in.nextLine());
-        int[] nums = new int[num];
-        for(int i = 0; i< num; i++){
-            nums[i] = Integer.parseInt(in.nextLine());
-        }
-        List<String> res =  summaryRanges(nums);
+        int res =  countPrimes(num);
         System.out.println(res);
     }
     

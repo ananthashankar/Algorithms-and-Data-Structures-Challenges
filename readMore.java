aa709b51c0376267f6488afee9eaa347bf338c57
ananthashankar <anantha.shankar1990@gmail.com>
2015-09-18 16:57:06 -0400

    September 18

---
+++ readMore.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Solution {
+    
+    public static String readMore(String s) {
+        String res = "";
+        if(s.length() == 0){
+            return res;
+        }
+        if(s.length() <= 55){
+            return s;
+        }
+        int i = 39;
+        
+        for(; i >= 0; i--){
+            if(s.charAt(i) == ' '){
+                break;
+            }
+        }
+        if(i == -1){
+            i = 40;
+        }
+        res = s.substring(0,i) + "... <Read More>";
+        
+        return res.trim();
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String res =  readMore(s);
+        System.out.println(res);
+    }
+}
